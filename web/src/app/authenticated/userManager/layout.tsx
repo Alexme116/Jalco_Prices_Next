63272601d5968099d3001b5cdcd236421d1e38d7
Alexme116 <alexmoncada16@hotmail.com>
2025-08-09 20:16:18 -0600

    Checking User Permissions in All Routes

--- web/src/app/authenticated/userManager/layout.tsx
+++ web/src/app/authenticated/userManager/layout.tsx
@@ -3,6 +3,8 @@
 import "../../globals.css";
 import { useRouter, usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
+import { useUser } from "@/context/UserContext";
+import { ProgressSpinner } from "@/icons/Icons";
 import UserManagerNav from "@/components/UserManagerNav";
 
 export default function RootLayout({
@@ -10,25 +12,43 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const router = useRouter();
-    const pathname = usePathname();
+    const { user, loading } = useUser();
     const [windowChanged, setWindowChanged] = useState(false)
     const [actualWindow, setActualWindow] = useState<string>("")
+
+    const router = useRouter();
+    const pathname = usePathname();
 
     const handleChangeView = (path: string) => {
         router.push(path);
         setWindowChanged(!windowChanged)
     }
 
+    // Update Actual Window
     useEffect(() => {
         setActualWindow(pathname)
     }, [pathname])
+
+    // Check User Permissions
+    useEffect(() => {
+        if (user) {
+            if (user.rol != "admin") {
+                window.location.href = "/authenticated";
+            }
+        }
+    }, [user]);
 
     return (
         <section className="p-5 flex flex-col gap-10">
             <UserManagerNav actualWindow={actualWindow} handleChangeView={handleChangeView} />
 
-            {children}
+            {loading ? (
+                <div className="flex justify-center">
+                    <ProgressSpinner />
+                </div>
+            ) : (
+                children
+            )}
         </section>
     );
 }
